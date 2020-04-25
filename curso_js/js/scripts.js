33048de35f83004adea305c06d54d2b458f60cef
Gabriel Marques <bielmarques922@gmail.com>
2020-04-25 11:53:10 -0300

    Variaveis

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -6,9 +6,18 @@ console.log("Mensagem enviada pelo console");
 */
 
 
-
 /* // Aula 02 - Sintaxe 
-
 Aula que falou sobre comentarios o uso do ; no final de cada linha de codigo
-
 */
+
+
+/* // Aula 03 - Variáveis
+
+var latitude = 40.87663;
+var longitude = -8.08373; 
+
+console.log(latitude);
+console.log(longitude);
+*/
+
+/* // Aula 04 - Tipos de dados primitivos - Strings */
