7a4d96d8d73f309dd3238a2a61319d7b11307a60
Gabriel Marques <bielmarques922@gmail.com>
2020-05-07 17:22:03 -0300

    Tipos de dados primitivos - Booleanos

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -9,35 +9,3 @@ console.log("Mensagem enviada pelo console");
 /* // Aula 02 - Sintaxe 
 Aula que falou sobre comentarios o uso do ; no final de cada linha de codigo
 */
-
-
-/* // Aula 03 - Variáveis
-
-var latitude = 40.87663;
-var longitude = -8.08373; 
-
-console.log(latitude);
-console.log(longitude);
-*/
-
-
-/* // Aula 04 - Tipos de dados primitivos - Strings 
-var nome = "João";  // podem ser usadas aspas duplas
-var sobrenome = 'Gomes';  // ou aspas simples
-var cpf = '111.111.111-11'; // Para numeros que não são feitos calculos e melhor usar string
-var telefone = "998887655";
-var ddd = '21';
-var email = 'joao@gmail.com';
-var biografia = "João começou a sua carreira como desenvolvedor web em 2010 e já participou de importantes projetos, como...";
-
-console.log('Nome completo: ' + nome + ' ' + sobrenome);
-
-console.log('(' + ddd + ')' + telefone);
-
-console.log('Primeira letra do nome:' + nome[0]);
-console.log('Primeira letra do nome:' + nome[3]);
-
-console.log('Número de caracteres do telefone: ' + telefone.length);
-
-// String é considerado uma sequencia de caracteres [0, 1, 2, 3] = "João"
-*/
